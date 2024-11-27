a2852a00394bb25f5f26bdeb62401f76b4e7bc9f
Parth-sk <parthsivakumar1@gmail.com>
2024-11-27 12:29:59 +0530

    1

--- src/main/java/com/convocraft/MessageSender.java
+++ src/main/java/com/convocraft/MessageSender.java
@@ -14,71 +14,7 @@ class MessageSender implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Start sending messages:@startuml
-        skinparam nodesep 50
-        skinparam ranksep 50
-        
-        class Chatroom {
-          - userIpMap: HashMap<String, String>
-          - cmdProcessor: CommandProcessor
-          - session: Session
-          - producer: MessageProducer
-          - consumer: MessageConsumer
-          + sendMessage(String message)
-          + receiveMessage()
-        }
-        
-        class User {
-          - username: String
-          - chatroom: Chatroom
-          + receiveMessage()
-          + sendMessage(String message)
-        }
-        
-        class Admin extends User {
-          + Admin(String username, Chatroom chatroom)
-        }
-        
-        class MessageSender {
-          - user: User
-          - scanner: Scanner
-          + run()
-        }
-        
-        class MessageReceiver {
-          - user: User
-          + run()
-        }
-        
-        class ActiveMQConnection {
-          - connectionFactory: ConnectionFactory
-          - connection: Connection
-          - session: Session
-          - destination: Destination
-          + getSession()
-          + getDestination()
-        }
-        
-        class ActiveMQConnectionManager {
-          + createChatroomTopic(String topicName)
-          + joinChatroomTopic(String topicName, String hostIp, String hostPort)
-        }
-        
-        class App {
-          + main(String[] args)
-        }
-        
-        Chatroom --* User
-        User --* Chatroom
-        Admin --* Chatroom
-        MessageSender --* User
-        MessageReceiver --* User
-        ActiveMQConnection --* ActiveMQConnectionManager
-        ActiveMQConnectionManager --* Chatroom
-        App --* Chatroom
-        App --* User
-        
-        @enduml");
+        System.out.println("Start sending messages: ");
         while (true) {
             String message = scanner.nextLine();
             user.sendMessage(message);
